cd7c82d71a4ae9f27162a2ff453cbf10b2eac6f0
Caz Yokoyama <caz@caztech.com>
2021-01-31 19:21:58 -0800

    When Heltec CubeCell is connected to Kobo mini, it is powered through Kobo's tty port which has 3.3v on V. CubeCell detects it as 2.8V. It is lower than BATTERY_CUTOFF_LIPO(3.2v). As a result, CubeCell goes to shutdown.
    Signed-off-by: Caz Yokoyama <caz@caztech.com>

--- software/firmware/source/SoftRF/src/driver/Battery.cpp
+++ software/firmware/source/SoftRF/src/driver/Battery.cpp
@@ -59,8 +59,7 @@ float Battery_cutoff()
   return hw_info.model == SOFTRF_MODEL_PRIME_MK2                             ||
         (hw_info.model == SOFTRF_MODEL_STANDALONE && hw_info.revision == 16) || /* TTGO T3 V2.1.6 */
          hw_info.model == SOFTRF_MODEL_DONGLE                                ||
-         hw_info.model == SOFTRF_MODEL_BADGE                                 ||
-         hw_info.model == SOFTRF_MODEL_MINI      ? BATTERY_CUTOFF_LIPO   :
+         hw_info.model == SOFTRF_MODEL_BADGE     ? BATTERY_CUTOFF_LIPO   :
          hw_info.model == SOFTRF_MODEL_UNI       ? BATTERY_CUTOFF_NIZNX2 :
                                                    BATTERY_CUTOFF_NIMHX2;
 }
